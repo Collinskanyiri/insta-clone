44c949a3c8213f774a6ec4646dba0a81a8f0d976
Collins Kanyiri <collins.kanyiri@student.moringaschool.com>
2021-10-31 17:19:00 +0300

    add forms

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -3,4 +3,14 @@ from django import forms
 from django.contrib.auth.models import User
 from mainapp.models import *
 
+class UpdateProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields=('user','username','bio','profile_photo')
+        exclude=['user']
 
+class CreateNewPostForm(ModelForm):
+    class Meta:
+        model=Image
+        fields='__all__'
+        exclude=['user', 'likes','profile','comments']
