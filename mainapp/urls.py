a2ae02753c7c9cdcefaaab0f237e0d4c309b786c
Collins Kanyiri <collins.kanyiri@student.moringaschool.com>
2021-10-31 17:25:35 +0300

    add profile url

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,7 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-   
+path('', views.index, name='home'),
+path('profile/', views.profile, name='profile'),
+path('upload/', views.save_image, name='save_img'),
+path('like/<int:id>/', views.like_image, name='like'),
 
 ]
 
