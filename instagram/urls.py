d9f78cdd2fd8dcd09a7b75b158c9ba5d83c994c8
Collins Kanyiri <collins.kanyiri@student.moringaschool.com>
2021-10-31 17:11:00 +0300

    add links

--- instagram/urls.py
+++ instagram/urls.py
@@ -17,5 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    
+    path('admin/', admin.site.urls),
+    path('', include('mainapp.urls')),
+    path('user/', include('user_auth.urls')),
+    path('user/', include('django.contrib.auth.urls')),
+    path('tinymce/', include('tinymce.urls')),
 ]
